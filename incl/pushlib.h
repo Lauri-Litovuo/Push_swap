334df9dca0a90157be408ff185ddc85d5e37950d
Lauri Litovuo <llitovuo@student.hive.fi>
2024-01-04 14:30:53 +0200

    adding stdlib to pushlib

--- incl/pushlib.h
+++ incl/pushlib.h
@@ -6,17 +6,18 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 15:01:54 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/01/03 15:02:58 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/01/04 12:13:02 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSHLIB_H
 # define PUSHLIB_H
 # include <stdarg.h>
+# include <stdlib.h>
 
 typedef struct s_stack
 {
-	void			*content;
+	int			*content;
 	int				size;
 	struct s_stack	*next;
 }				t_stack;
