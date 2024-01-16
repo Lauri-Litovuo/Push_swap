e6e4347210fbaaf8c7d259dafecf1de5eebe18a6
Lauri Litovuo <llitovuo@student.hive.fi>
2024-01-16 13:32:41 +0200

    updating Makefile and pushlib.h

--- Makefile
+++ Makefile
@@ -16,13 +16,16 @@ CFLAGS += -Wall -Wextra -Werror -I$I
 LDFLAGS += 
 
 SRC = $Spush_swap.c \
-		$Sadd_and_del.c \
+		$Slinked_list_helpers.c \
 		$Spush_operations.c \
 		$Sreverse_operations.c \
 		$Srotation_operations.c \
 		$Sswap_operations.c \
 		$Scount_and_validate_input.c \
 		$Sinput_to_linklist.c \
+		$Smini_algo.c \
+		$Sgive_index.c \
+		$Smedium_algos.c \
 
 OBJ = $(SRC:$S%=$O%.o)
 
@@ -38,6 +41,22 @@ $O%.o: $S% $(LIBFT)
 $(NAME): $(LIBFT) $(OBJ)
 	@$(CC) $(CFLAGS) $^ -o $@
 	@echo "Project ready for use."
+
+## delete this before eval
+DEBUG_NAME = debug.out
+
+debug: $(DEBUG_NAME)
+
+DEBUG_FLAGS = -g -fsanitize=address,undefined,integer
+
+$(DEBUG_NAME): $(LIBFT) $(OBJ)
+	@$(CC) $(DEBUG_FLAGS) $^ -o $@
+	@echo "Debug ready for use."
+
+cleandebug: fclean
+	@rm -f $(DEBUG_NAME)
+	@echo "debug.out removed"
+## to here
 
 $(LIBFT): $L
 	@make -C $L
--- incl/pushlib.h
+++ incl/pushlib.h
@@ -6,7 +6,7 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 15:01:54 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/01/13 19:14:15 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/01/16 10:11:04 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@
 typedef struct s_stack
 {
 	int				content;
-	int				size;
+	int				index;
 	struct s_stack	*next;
 }				t_stack;
 
 t_stack	*ft_stacknew(int content);
 void	ft_stackadd_front(t_stack **stack, t_stack *new);
 void	ft_stackadd_back(t_stack **stack, t_stack *new);
+int		ft_stacksize(t_stack *stack);
 void	push_ab(t_stack **stack_from, t_stack **stack_to, char ch);
 void	swap_ab(t_stack **stack, char ch);
 void	swap_simult(t_stack **stack_a, t_stack **stack_b);
@@ -38,6 +39,9 @@ t_stack	*make_stack(t_stack	**stack, char **elem_list, int count);
 t_stack	*input_to_list(int ac, char **av);
 int		count_elements(char *str);
 int		validate_input(char **elem_list, int count);
-void	mini_algo(t_stack **stack_a);
+void	give_index(t_stack **stack_ab);
+void	sort_three(t_stack **stack_a);
+void	sort_four(t_stack **stack_a, t_stack **stack_b);
+void	sort_five(t_stack **stack_a, t_stack **stack_b);
 
 #endif
